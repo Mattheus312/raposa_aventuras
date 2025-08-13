04b1c664ca7d6871cd9b86194c8e7946d0f6e8bc
CHANTEPIE Matthieu <matthieu.chantepie@wavestone.com>
2025-08-13 21:49:21 +0200

    Build PWA pour déploiement

---
+++ dist/raposa-aventuras/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Mattheus312.github.io/raposa_aventuras',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
